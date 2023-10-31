9d1d7f4b655a3ad85cd657aaacddd0698166af95
Schpudt <hikaru.kp@gmail.com>
2023-10-31 18:12:36 +0700

    +Edit URL

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -41,15 +41,13 @@ const config = {
           sidebarPath: require.resolve("./sidebars.js"),
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
-          editUrl:
-            "https://github.com/Joskul/cpe101-viewbook/tree/main/packages/create-docusaurus/templates/shared/",
+          editUrl: "https://github.com/Joskul/cpe101-viewbook/blob/main/docs/",
         },
         blog: {
           showReadingTime: true,
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
-          editUrl:
-            "https://github.com/Joskul/cpe101-viewbook/tree/main/packages/create-docusaurus/templates/shared/",
+          editUrl: "https://github.com/Joskul/cpe101-viewbook/blob/main/blog/",
         },
         theme: {
           customCss: require.resolve("./src/css/custom.css"),
